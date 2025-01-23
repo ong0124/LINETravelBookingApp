d2d11a93011d4f3955e3ecb636f2b0a9e709a487
unknown <l.t.ong0124@gmail.com>
2025-01-23 17:18:52 +0800

    update i18n

--- components/PopUpRules.vue
+++ components/PopUpRules.vue
@@ -12,21 +12,33 @@
       <!-- 彈出框 -->
       <div
         v-if="showPopup"
-        class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
-      >
+        class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10"
+       >
         <div class="bg-white p-6 rounded-lg shadow-lg max-w-sm">
-          <h2 class="text-sm font-bold mb-4">{{ $t('HomePage.terms') }}</h2>
-          <p class="text-xs text-gray-700">
-            條款與細則的內容.......，請仔細閱讀。
-            -----------------------------------
-            ------------------------------------
-            ------------------------------------
-          </p>
+          <h2 class="text-lg font-bold mb-4">{{ $t('HomePage.terms') }}</h2>
+          <div class="text-xs text-gray-700">
+            <p class="text-base font-medium">{{ $t('PopUpRules.title1') }}</p>
+              <p>{{ $t('PopUpRules.general_ticket') }}</p>
+              <p>{{ $t('PopUpRules.infant_ticket') }}</p>
+              <p class="text-base font-medium">{{ $t('PopUpRules.title2') }}</p>
+              <p>{{ $t('PopUpRules.notice') }}</p>
+              <p>{{ $t('PopUpRules.real_info') }}</p>
+              <p>{{ $t('PopUpRules.online_deadline') }}</p>
+              <p>{{ $t('PopUpRules.refund_deadline') }}</p>
+              <p>{{ $t('PopUpRules.ticket_validity') }}</p>
+              <p>{{ $t('PopUpRules.contact') }}</p>
+              <p class="text-base font-medium">{{ $t('PopUpRules.title3') }}</p>
+              <p>{{ $t('PopUpRules.process') }}</p>
+              <p>{{ $t('PopUpRules.safety') }}</p>
+              <p>{{ $t('PopUpRules.valuables') }}</p>
+              <p>{{ $t('PopUpRules.insurance') }}</p>
+              <p>{{ $t('PopUpRules.invoice') }}</p>
+          </div>
           <button
             class="mt-4 px-4 py-2 bg-green-300 text-white rounded hover:bg-green-500"
             @click="showPopup = false"
           >
-            關閉
+          {{ $t('okButton') }}
           </button>
         </div>
       </div>
